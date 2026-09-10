7afe211fd91af32eb3d1c872123c5c4c47ba9a65
vladyiv <ivanovavs23@st.ithub.ru>
2026-09-10 14:16:02 +0300

    Добавила 6-е задание

---
+++ 6)CatchThrowWhen/6)CatchThrowWhen/Program.cs
@@ -0,0 +1,48 @@
+﻿// Вариант 1. Парсер списка чисел
+
+namespace _6_CatchThrowWhen
+{
+    internal class Program
+    {
+        public static int SumValidNumbers(List<string> values)
+        {
+            int sum = 0, count = 0;
+            foreach (string value in values)
+            {
+                count++;
+                try
+                {
+                    Console.WriteLine($"{sum} + {int.Parse(value)}");
+                    sum += int.Parse(value);
+                    Console.WriteLine($"Сумма: {sum}.");
+
+                }
+                catch (FormatException) when (string.IsNullOrWhiteSpace(value))
+                {
+
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Обнаружен элемент неверного формата!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine("Ошибка: переполнение int!");
+                    throw;
+                }
+                finally
+                {
+                    Console.WriteLine($"{count}-й обработанный элемент");
+                }
+            }
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Первый массив");
+            Console.WriteLine(SumValidNumbers(["10", "", "abc", "20"])); // возвращает 30
+            Console.WriteLine("\nВторой массив");
+            Console.WriteLine(SumValidNumbers(["99999999999", "1", "abc"])); // срабатывает ошибка на первом элементе, и программа даже не доходит до следующих элементов
+        }
+    }
+}
