a0382f333e659aefc048d3bcabc276eed99df306
Влада Иванова <IvanovaVS23@st.ithub.ru>
2026-09-12 18:10:20 +0300

    Update InsufficientFundsException.cs

--- 7-1)CustomExceptions/7-1)CustomExceptions/InsufficientFundsException.cs
+++ 7-1)CustomExceptions/7-1)CustomExceptions/InsufficientFundsException.cs
@@ -7,7 +7,7 @@ namespace _7_1_CustomExceptions
     internal class InsufficientFundsException : BankingException
     {
         public decimal RequestedAmount, AvailableBalance;
-        public InsufficientFundsException(decimal requestedAmount, decimal availableBalance) : base($"Сумма вывода больше баланса! RequestedAmount = {requestedAmount}, AvailableBalance = {availableBalance}.")
+        public InsufficientFundsException(decimal requestedAmount, decimal availableBalance) : base($"Сумма вывода больше баланса! RequestedAmount = {requestedAmount} у.е, AvailableBalance = {availableBalance} у.е.")
         {
             this.RequestedAmount = requestedAmount;
             this.AvailableBalance = availableBalance;
